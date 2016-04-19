3808ca9798e478f4c2f28fe9139ee97cbade9d2a
John Dow and Tommy Jones <tommy@>
2016-04-19 10:56:55 -0700

    Now Factors in test to check if lower and upper case still work

--- src/test/java/ScrabbleScoreTest.java
+++ src/test/java/ScrabbleScoreTest.java
@@ -6,11 +6,16 @@ public class ScrabbleScoreTest {
   @Test
   public void calculateScrabbleScore_ReturnScoreForSingleLetter_1(){
     ScrabbleScore testScrabbleScore = new ScrabbleScore();
-    assertEquals((Integer)1, testScrabbleScore.calculateScrabbleScore("A"));
+    assertEquals((Integer)1, testScrabbleScore.calculateScrabbleScore("a"));
   }
   @Test
   public void calculateScrabbleScore_SumofLetters_Integer() {
     ScrabbleScore testScrabbleScore = new ScrabbleScore();
     assertEquals((Integer)4, testScrabbleScore.calculateScrabbleScore("aga"));
+  }
+  @Test
+  public void calculateScrabbleScore_ReturnForUpperandLowercase_Integer(){
+    ScrabbleScore testScrabbleScore = new ScrabbleScore();
+    assertEquals((Integer)1, testScrabbleScore.calculateScrabbleScore("A"));
   }
 }
